afae23266f038f278e390f6d15201a51aadd4404
rmontone <rmontone19@gmail.com>
2023-09-12 22:25:42 -0300

    add adquirido

--- src/components/CadFilme.jsx
+++ src/components/CadFilme.jsx
@@ -6,13 +6,22 @@ export default function CardFilme({filme}){
     const[favorito,setFavorito] = useState(false)
     var id = filme.id.replace('-', '/')
     const url_imagem = `https://images.pokemontcg.io/${id}.png`
-
+    function favoritar(){
+     setFavorito(true)
+     
+    }
     return(
         <div id="card" className="flex flex-col w-40 justify-center items-center m-2">
-        { favorito ? 
+        { favorito ?
+        <div className="flex"> 
        <BookmarkIcon onClick={()=> setFavorito(false)} className="h-6 w-6 text-rose-600 cursor-pointer" />
+         adquirido
+        </div> 
         :
-        <BookmarkIcon onClick={() => setFavorito(true)} className="h-6 w-6 text-blue-500 cursor-pointer" />
+        <div className="flex"> 
+        <BookmarkIcon onClick={favoritar} className="h-6 w-6 text-blue-500 cursor-pointer" />
+            nao adquirido
+        </div>
         }
         
         <img className="rounded"src={url_imagem} alt="" />
@@ -20,8 +29,9 @@ export default function CardFilme({filme}){
             {filme.name}</span>
             
         <div>
-          <span>{filme.artist} 
-            {filme.rarity} </span>
+          Artista: <span>{filme.artist} 
+          <br></br>
+           Raridade: {filme.rarity} </span>
         </div>
         <a href="#" className="bg-pink-600 py-2 w-full rounded text-center">detalhes</a>
      </div>    
