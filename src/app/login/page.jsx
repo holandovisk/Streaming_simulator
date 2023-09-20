ce9674aeae678e9f6962c8b397352a2e76589e97
rmontone <rmontone19@gmail.com>
2023-09-19 22:35:59 -0300

    criando /login

---
+++ src/app/login/page.jsx
@@ -0,0 +1,40 @@
+"use client"
+import Image from "next/image";
+import loginimage from "@/images/login.jpg"
+import { useState } from "react";
+export default function Login(){
+    const[email,setEmail] = useState("")
+    const[senha,setSenha] = useState("")
+    function login(e){
+        e.preventDefault()
+        console.log(email, senha)
+    }
+    return(
+        <div className="flex"> 
+            <aside className="hidden lg:flex h-screen">
+                <Image src={loginimage} className="h-auto w-auto object-cover"/>
+            </aside>
+            <main className="flex flex-col justify-center items-center p-4 h-screen w-full">
+                <h2>Pokemon TCG Cards</h2>
+                <form onSubmit={login} className="flex flex-col gap-2">
+                    <label htmlFor="email">E-mail</label>
+                    <input  
+                        type="email" 
+                        id="email"
+                        value={email}
+                        onChange={(e)=> setEmail(e.target.value)}
+                        className="bg-slate-600 p-1 rounded"/>
+                    <label htmlFor="password">Senha</label>
+                    <input 
+                        type="password" 
+                        id="password" 
+                        value={senha}
+                        onChange={(e)=> setSenha(e.target.value)}
+                        className="bg-slate-600 p-1 rounded"/>
+                    <button className="bg-pink-600 p-2 rounded mt-2">entrar</button>
+
+                </form>
+            </main>
+        </div>
+    )
+}
--- src/components/CadFilme.jsx
+++ src/components/CadFilme.jsx
@@ -1,5 +1,5 @@
 "use client"
-import { BookmarkIcon } from '@heroicons/react/24/outline'
+import { CheckCircleIcon } from '@heroicons/react/24/outline'
 import { useState } from 'react'
 import useFavorito from '@/hooks/filmes'
 
@@ -9,15 +9,15 @@ export default function CardFilme({filme}){
     const url_imagem = `https://images.pokemontcg.io/${id}.png`
    
     return(
-        <div id="card" className="flex flex-col w-40 justify-center items-center m-2">
+        <div id="card" className=" flex-col w-40 justify-center items-center m-2">
         { favorito ?
         <div className="flex"> 
-       <BookmarkIcon onClick={() => desfavoritar(filme)} className="h-6 w-6 text-rose-600 cursor-pointer " />
+       <CheckCircleIcon onClick={() => desfavoritar(filme)} className="h-6 w-6 text-rose-600 cursor-pointer " />
          adquirido
         </div> 
         :
         <div className="flex"> 
-        <BookmarkIcon onClick={() => favoritar(filme)}  className="h-6 w-6 text-slate-100 cursor-pointer" />
+        <CheckCircleIcon onClick={() => favoritar(filme)}  className="h-6 w-6 text-slate-100 cursor-pointer" />
             nao adquirido
         </div>
         }
@@ -27,9 +27,9 @@ export default function CardFilme({filme}){
             {filme.name}</span>
             
         <div>
-          Artista: <span>{filme.artist} 
+          Artista: {/*<span>*/}{filme.artist} 
           <br></br>
-           Raridade: {filme.rarity} </span>
+           {/*Raridade: {filme.rarity} </span>*/}
         </div>
         <a href="#" className="bg-pink-600 py-2 w-full rounded text-center">detalhes</a>
      </div>    
